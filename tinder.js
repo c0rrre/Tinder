02341483c42ce107cce7aaca40a840b49bdf1f5f
c0rrre <c0rrre@users.noreply.github.com>
2019-03-07 12:24:38 +0100

    Update tinder.js
    new website elements

--- tinder.js
+++ tinder.js
@@ -1,23 +1,6 @@
-let run = true, time_step = 1000;
-const wait = (n = 1) => new Promise((rs, rj) => run ? setTimeout(rs, n) : rj()),
-	click = async (cn, i = 0) => {
-		document.getElementsByClassName(cn)[i].click();
-		return wait(time_step)
-	};
-document.onkeydown = (e = window.event) => {
-	if (e.key === "a") run = run ? true : explore() || true;
-	else if (e.key === "z") console.log(time_step *= 0.9);
-	else if (e.key === "x") console.log(time_step *= 1.1);
-	else run = false;
-};
-const explore = async () =>
-	click("recCard__info").then(() =>
-		Array.from(document.getElementsByClassName("bullet")).reduce((p, e) =>
-			p.then(() => {
-				e.click();
-				return wait(time_step)
-			}), Promise.resolve())
-	).then(() =>
-		click("recsGamepad__button--like")
-	).then(() => explore());
-explore();
+a = setInterval(
+function(){
+var elem=
+document.getElementsByClassName("recsGamepad__button");
+elem[3].click()
+},2000)
